411cfdb88b2e6575e8c1f3ec930664f8d9ef2f52
Shadowru <proteyrus@gmail.com>
2020-10-12 01:20:18 +0300

    Messages

--- include/hoverboard_driver/hoverboard_driver.h
+++ include/hoverboard_driver/hoverboard_driver.h
@@ -1,4 +1,15 @@
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ros/ros.h>
+
+#include <geometry_msgs/Twist.h>
+#include <tf/transform_broadcaster.h>
+#include <nav_msgs/Odometry.h>
+
 #include "uart_peripheral.h"
+
+#include "hoverboard_driver/serial.h"
 #include "hoverboard_driver/hoverboard_msg.h"
 
 class hoverboard_driver : public uart_peripheral
--- src/hoverboard_driver_node.cpp
+++ src/hoverboard_driver_node.cpp
@@ -1,13 +1,4 @@
-#include <vector>
-#include <stdio.h>
-#include <stdlib.h>
-#include <ros/ros.h>
-
-#include <geometry_msgs/Twist.h>
-#include <tf/transform_broadcaster.h>
-#include <nav_msgs/Odometry.h>
-
-#include "hoverboard_driver/serial.h"
+#include "hoverboard_driver/hoverboard_driver.h"
 
 using namespace std;
 
