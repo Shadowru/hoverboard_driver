c7db958c3b0955cbf99680f04fbe79092fe42e32
Shadowru <proteyrus@gmail.com>
2020-10-12 01:21:36 +0300

    Messages

--- include/hoverboard_driver/hoverboard_driver.h
+++ include/hoverboard_driver/hoverboard_driver.h
@@ -7,16 +7,6 @@
 #include <tf/transform_broadcaster.h>
 #include <nav_msgs/Odometry.h>
 
-#include "uart_peripheral.h"
-
 #include "hoverboard_driver/serial.h"
 #include "hoverboard_driver/hoverboard_msg.h"
 
-class hoverboard_driver : public uart_peripheral
-{
-public:
-    explicit hoverboard_driver(char* device);
-    virtual ~hoverboard_driver();
-private:
-    void velCallback(const geometry_msgs::Twist &vel);
-};
