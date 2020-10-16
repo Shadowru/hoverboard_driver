fd02bb0add77daaa6e7b697af5356e83e4fc996e
Shadowru <proteyrus@gmail.com>
2020-10-16 12:29:23 +0300

    Odometry

--- src/hoverboard_driver_node.cpp
+++ src/hoverboard_driver_node.cpp
@@ -240,7 +240,7 @@ int main(int argc, char **argv) {
     ROS_INFO("Starting hoverboard_driver node");
     ros::init(argc, argv, "hoverboard_driver");
     ros::NodeHandle node;
-    ros::Rate rate(1);  // 100 hz
+    ros::Rate rate(50);  // 100 hz
 
     std::string hoverboard_uart;
 
